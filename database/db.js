d6677cbf10e4efd4fc86d8fb48d841dedbd3e479
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-02-06 07:58:39 +0000

    transferring files from earlier project folder

---
+++ database/db.js
@@ -0,0 +1,16 @@
+const Database = require('better-sqlite3');
+const { readFileSync } = require('node:fs');
+const { join } = require('node:path');
+
+const db = new Database(process.env.DB_FILE);
+
+const schemaPath = join('database', 'schema.sql');
+const schema = readFileSync(schemaPath, 'utf-8');
+db.exec(schema);
+
+const select_table = db.prepare("SELECT name FROM sqlite_schema");
+const result = select_table.all();
+
+console.log(result);
+
+module.exports = db;
---
+++ model/tasks.js
@@ -0,0 +1,16 @@
+const db = require('../database/db.js');
+
+const insert_task = db.prepare(`
+    INSERT INTO tasks (content) 
+    VALUES (?)
+    RETURNING id, content, created_at
+`);
+
+function createTask(content) {
+    return insert_task.get(content);
+}
+
+module.exports = { createTask };
+
+const result = createTask("Send mum flowers");
+console.log(result);
