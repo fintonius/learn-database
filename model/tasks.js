483a2670bada61fa8594fc41dc910fa01b66bcfd
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-02-06 08:16:35 +0000

    adds new function to tasks.js

--- model/tasks.js
+++ model/tasks.js
@@ -1,13 +1,28 @@
 const db = require('../database/db.js');
 
+// the VALUES property(?) takes info from the argument passed to it
+// when the createTask() function is called if it's passed an object
+// as the argument, ie: createTask({ content: "stuff", complete: 1 })
+// the statement will map each key to the corresponding $param in 
+// the query
+// ...I think...
 const insert_task = db.prepare(`
-    INSERT INTO tasks (content) 
-    VALUES (?)
+    INSERT INTO tasks (content, complete)
+    VALUES ($content, $complete)
     RETURNING id, content, created_at
 `);
 
-function createTask(content) {
-    return insert_task.get(content);
+function createTask(task) {
+    return insert_task.get(task);
+}
+
+const select_tasks = db.prepare(
+    // sql
+    `SELECT id, content, created_at, complete FROM tasks`
+);
+
+function listTasks() {
+    return select_tasks.all();
 }
 
 module.exports = { createTask };
