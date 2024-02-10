57e9d9aa5c93655b49fd3ee4bcc5b1e6e5661a90
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-02-10 13:05:04 +0000

    update code

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const server = require('./server.js');
 
 const PORT = process.env.PORT || 3030;
 
-server.listen(PORT, () => console.log(`creeping on http://localhost:${PORT}`))
+server.listen(PORT, () => console.log(`creeping on http://localhost:${PORT}`));
--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require("express");
+const model = require("./model/tasks.js");
 
 const server = express();
 
 server.get("/", (req, res) => {
     res.send('hell world');
+
+    const body = `
+    <!doctype html>
+    <form method="POST">
+        <input id="content" name="content" aria-label="New task" required>
+        <button>Add task +</button>
+    </form>`;
+    res.send(body)
+});
+
+server.post("/", express.urlencoded({ extended: false }), (req, res) => {
+    const task = {
+        content: req.body.content,
+        complete: 0,
+    };
+    model.createTask(task);
+    res.redirect("/");
 });
 
 module.exports = server;
